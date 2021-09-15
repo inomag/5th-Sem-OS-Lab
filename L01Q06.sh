2c1a5e2a543f8866c33ff639ad57e50be5cccdf3
Mɪᴇɢʀᴀᴛ Sᴀᴍᴍʀɪ <55135657+GrimScythe2001@users.noreply.github.com>
2021-09-15 22:48:03 +0545

    Update L01Q06.sh

--- L01Q06.sh
+++ L01Q06.sh
@@ -8,9 +8,32 @@ for fileName in *
 do
 	if [ -f $fileName ]
 	then
-		# echo $fileName | tr [:lower:] [:upper:]
-		tr [:lower:] [:upper:] < $fileName
+		if [[ $fileName != *.sh ]]
+		then
+			awk '{print}' $fileName | tr [:lower:] [:upper:] > "tmp"$fileName
+			rm $fileName
+			mv "tmp"$fileName $fileName
+		fi
 	fi
 done
+# $chmod +x L01Q06.sh
+# $./L01Q06.sh
+
+
+
+# 1912160_L01Q06.sh
+
+# Q.6. Write a shell script that accepts one or more file name as arguments and converts all of them to uppercase, provided they exist in the current directory.
+
+# $nano L01Q06.sh
+
+# for fileName in *
+# do
+#	if [ -f $fileName ]
+#	then
+#		# echo $fileName | tr [:lower:] [:upper:]
+#		tr [:lower:] [:upper:] < $fileName
+#	fi
+# done
 # $chmod +x L01Q06.sh
 # $./L01Q06.sh
