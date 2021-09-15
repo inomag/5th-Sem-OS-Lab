93e2f6b0792d42ecd76c95bf9bddba5172a10846
Mɪᴇɢʀᴀᴛ Sᴀᴍᴍʀɪ <55135657+GrimScythe2001@users.noreply.github.com>
2021-09-15 23:03:03 +0545

    Update L01Q10.sh

--- L01Q10.sh
+++ L01Q10.sh
@@ -7,10 +7,16 @@
 echo "Enter Word for which the lines are to be deleted: "
 read word
 echo "$word containing lines will be deleted, wherever applicable"
-for file in *
+for fileName in *
 do
-	echo "File Name: "$file
-	grep -iv $word $file
+	if [[ $fileName != *.sh ]]
+	then
+		echo "File Name: "$fileName
+		grep -iv $word $fileName > "temp"$fileName
+		rm $fileName
+		mv "temp"$fileName $fileName
+		
+	fi
 done
 
 # $chmod +x L01Q10.sh
